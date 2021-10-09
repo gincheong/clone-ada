3ee9baff703a8ce7ca98c6e2d8d5592d58cc7901
gincheong2 <gincheong2@gmail.com>
2021-10-10 00:53:31 +0900

    feat(redux): redux 적용

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+// store
+import store from './src/stores';
 // screens
 import MainScreen from './src/screens/MainScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
@@ -14,7 +17,7 @@ const Stack = createNativeStackNavigator<StackProps>();
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Main">
@@ -27,7 +30,7 @@ export default function App() {
           <Stack.Screen name="Setting" component={SettingScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </Provider>
   );
 }
 
