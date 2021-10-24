87a4cc72a7f0bec8c48489baeb11665f03df7c00
gincheong2 <gincheong2@gmail.com>
2021-10-24 21:40:34 +0900

    feat(Globalize): ProfileScreen의 UI 번역 추가

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
+import { useGlobalize } from 'react-native-globalize';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 // components
 import ListItem from '../components/ListItem/ListItem';
 // interfaces
 import { StackProps } from './interfaces';
 import { Profile } from '../asyncStorage/interfaces';
+// globalize
+import loadProfileMessages from '../globalize/ProfileMessages';
 // storage
 import { getStorageData } from '../asyncStorage/AsyncStorage';
 // styles
 import theme from '../assets/theme';
 
+loadProfileMessages();
 const iconSize = theme.font.xlarge;
 
 const ProfileScreen = ({
   navigation,
 }: StackScreenProps<StackProps, 'Profile'>) => {
   const [profiles, setProfiles] = React.useState<Profile[]>([]);
+  const { formatMessage } = useGlobalize();
 
   // * Load AsyncStorage
   React.useEffect(() => {
@@ -32,7 +37,7 @@ const ProfileScreen = ({
     <View style={styles.container}>
       <ListItem
         icon={<FeatherIcon name="plus" size={iconSize} />}
-        title={'Add New Profile'}
+        title={formatMessage('profile/add')}
       />
       {profiles.map(each => (
         <ListItem
